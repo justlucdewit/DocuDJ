c1675880043da4dd803c7edb212c4ace90bf4387
Luc de Wit <luc.dewit@hotmail.nl>
2023-01-15 23:43:51 +0100

    attempt download as pdf

--- src/components/Hierarchy.vue
+++ src/components/Hierarchy.vue
@@ -6,7 +6,7 @@
       :item="item"
     />
 
-    <span class="download-pdf-wrapper" v-if="config.settings.downloadAsPDF">
+    <span class="download-pdf-wrapper" v-if="config.settings.downloadAsPDF" @click="downloadAsPDF()">
       <hr />
       <span class="download-pdf">
         <span class="material-symbols-outlined">
@@ -22,6 +22,9 @@
 
 <script>
 import HierarchyItem from './HierarchyItem.vue'
+import jsPDF from 'jspdf'
+import marked from 'marked'
+import axios from 'axios'
 
 export default {
     data: () => ({
@@ -31,6 +34,59 @@ export default {
 
     components: {
         HierarchyItem
+    },
+
+    methods: {
+      async downloadAsPDF() {
+        const doc = new jsPDF();
+        const docName = this.config.downloadDocName ?? 'download.pdf';
+        let html = "";
+
+        this.content.forEach(async page => {
+          const content = (await axios.get('/docs/' + page.file)).data;
+          const htmlContent = this.render(content, page);
+          html += htmlContent;
+        });
+
+        doc.html(html, {
+          callback: function(doc) {
+              // Save the PDF
+              doc.save(docName);
+          },
+          margin: [10, 10, 10, 10],
+          autoPaging: 'text',
+          x: 0,
+          y: 0,
+          width: 190, //target width in the PDF document
+          windowWidth: 675 //window width in CSS pixels
+        });
+      },
+
+      render(content, contentItem) {
+        let html = marked.parse(content);
+
+        // parse html string to actual html
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(html, "text/html");
+        const wordCount = doc.body.textContent.split(' ').length;
+
+        // Get the first h1
+        let firstH1 = doc.querySelector('h1');
+
+        const injectedContent = `
+            <span class='article-info'>
+                Article •
+                ${contentItem.date ? contentItem.date + ' • ' : ''}
+                ± ${Math.ceil(wordCount / 180)} minutes to read
+            </span>
+        `;
+
+        // Inject the content after the first h1
+        firstH1.insertAdjacentHTML('afterend', injectedContent);
+
+        // Convert back to string and return
+        return doc.body.innerHTML;
+      }
     }
 }
 </script>
